a68455f0eb216c58dc0cadf3aba4a28ceb8235aa
danny60106 <54652877+danny60106@users.noreply.github.com>
2020-07-14 16:14:56 +0800

    Create KNN.py

---
+++ KNN.py
@@ -0,0 +1,7 @@
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
